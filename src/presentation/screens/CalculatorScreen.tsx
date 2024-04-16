75c83959d04b48bb06b9b76f84dbc70b9eef1109
Pedro <pedrohm92@gmail.com>
2024-04-16 14:14:09 +0200

    Creación de estilos básicos

---
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -0,0 +1,18 @@
+import { Pressable, Text, View } from "react-native"
+import { styles } from "../../config/theme/app-theme"
+
+export const CalculatorScreen = () => {
+  return (
+    <View style={styles.calculatorContainer}>
+        <View style={{ paddingHorizontal: 30, paddingBottom: 20 }}>
+            <Text style={styles.mainResult}>1500</Text>
+            <Text style={styles.subResult}>15</Text>
+        </View>
+        <View style={styles.row}>
+            <Pressable style={styles.button}>
+                <Text style={styles.buttonText}>Hola Mundo</Text>
+            </Pressable>
+        </View>
+    </View>
+  )
+}
